3b3e055b067b4008f12667760e136c7d73a4b8d2
SALOMAO VALENTIM DE OLIVEIRA DOS ANJOS <392950492@alunos.etec.itaqua>
2019-03-28 22:26:21 -0300

    Alterei a pagina de detalhes da cerveja, botão e construtor.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'menu', loadChildren: './home/home.module#HomePageModule' },
   { path: 'puro-malte', loadChildren: './puro-malte/puro-malte.module#PuroMaltePageModule' },
   { path: 'cervejas', loadChildren: './cervejas/cervejas.module#CervejasPageModule' },
   { path: 'pilsen', loadChildren: './pilsen/pilsen.module#PilsenPageModule' },
   { path: 'lager', loadChildren: './lager/lager.module#LagerPageModule' },
-  { path: 'cerveja-detalhes', loadChildren: './cerveja-detalhes/cerveja-detalhes.module#CervejaDetalhesPageModule' },
+  { path: 'cerveja-detalhes/:id', loadChildren: './cerveja-detalhes/cerveja-detalhes.module#CervejaDetalhesPageModule' },
 ];
 
 @NgModule({
